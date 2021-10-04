12f27606f5790c0d68f79fa1d4538537d2987290
Asep Erlan Maulana <ceperlan@Users-MacBook-Pro.local>
2021-10-04 13:12:11 +0700

    add buttons

--- lib/main.dart
+++ lib/main.dart
@@ -23,64 +23,114 @@ class MyHomePage extends StatelessWidget {
           title: Text('Home Page'),
         ),
         body: Center(
-          child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              // crossAxisAlignment: CrossAxisAlignment.center
-              children: [
-                Container(
-                  width: 300,
-                  height: 300,
-                  decoration: BoxDecoration(
-                    color: Colors.amber,
-                    borderRadius: BorderRadius.circular(30),
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage(
-                        'assets/adem.jpg',
+          child: Column(
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  // crossAxisAlignment: CrossAxisAlignment.center
+                  children: [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Container(
+                      width: 300,
+                      height: 300,
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(30),
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage(
+                            'assets/adem.jpg',
+                          ),
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'A.E.M',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
                       ),
                     ),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'A.E.M',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Container(
+                      width: 300,
+                      height: 300,
+                      // margin: EdgeInsets.symmetric(
+                      //   horizontal: 30,
+
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(30),
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage(
+                            'assets/adem.jpg',
+                          ),
+                        ),
                       ),
-                      textAlign: TextAlign.center,
+                      child: Center(
+                        child: Text(
+                          'A.E.M',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 60,
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.amber,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      )),
+                  child: Text(
+                    'Get Started',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
                     ),
                   ),
                 ),
-                Container(
-                  width: 300,
-                  height: 300,
-                  decoration: BoxDecoration(
-                    color: Colors.amber,
-                    borderRadius: BorderRadius.circular(30),
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage(
-                        'assets/adem.jpg',
-                      ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Terms & Conditions',
+                    style: TextStyle(
+                      color: Colors.purple,
+                      fontSize: 20,
                     ),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'A.E.M',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+                  ))
+            ],
           ),
         ),
 
